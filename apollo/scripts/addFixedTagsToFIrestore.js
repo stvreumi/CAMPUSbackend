55eb3be6f936aa6462eefa04ad8bc7fd5f0e9c09
Dennis Chung <stvreumi.cs04@g2.nctu.edu.tw>
2022-04-16 20:59:38 +0800

    feat: :hammer: Add fixedTag default data scripts

--- apollo/scripts/addFixedTagsToFIrestore.js
+++ apollo/scripts/addFixedTagsToFIrestore.js
@@ -1,9 +1,5 @@
 const { readFileSync } = require('fs');
 
-// https://github.com/brix/crypto-js#usage
-/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
-const SHA256 = require('crypto-js/sha256');
-
 const admin = require('firebase-admin');
 
 // Please set google credential in the env when running
@@ -21,20 +17,13 @@ const fixPoints = JSON.parse(
 // convert geo related data to geo type, also have geo hash function and field
 // give each information a unique uid, to reference status collection
 const fixPointsWithGeoPointsAndId = fixPoints.map(point => {
-  const { coordinates: originalCoordinates, information: originalInformation } =
-    point;
+  const { coordinates: originalCoordinates } = point;
   return {
     ...point,
     coordinates: new admin.firestore.GeoPoint(
       parseFloat(originalCoordinates.latitude),
       parseFloat(originalCoordinates.longitude)
     ),
-    information: originalInformation.map(item => ({
-      ...item,
-      id: SHA256(JSON.stringify({ originalCoordinates, item }))
-        .toString()
-        .substring(0, 8),
-    })),
   };
 });
 
@@ -46,7 +35,15 @@ console.dir(fixPointsWithGeoPointsAndId.map(point => point.information));
 // uploatd to collection `fixPoints`
 Promise.all(
   fixPointsWithGeoPointsAndId.map(async point => {
-    await firestore.collection('fixedTags').add(point);
+    const { locationName, coordinates, viewCount, information } = point;
+    const tagData = { locationName, coordinates, viewCount };
+    const tagRef = await firestore.collection('fixedTags').add(tagData);
+    return Promise.all(
+      information.map(async info => {
+        const subLocationData = { ...info, fixedTagId: tagRef.id };
+        await firestore.collection('fixedTagSubLocation').add(subLocationData);
+      })
+    );
   })
 ).catch(e => console.error(e));
 
